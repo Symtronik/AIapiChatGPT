d2f49792bb532d552439410ecedb2c4d3dc18d2b
staz <m.kobialka@healthpharm.pl>
2023-10-22 17:12:30 +0200

    Create function

--- chatGPT.py
+++ chatGPT.py
@@ -8,38 +8,23 @@ API_KEY = os.getenv("SECRET_KEY_CHATGPT")
 
 openai.api_key = API_KEY
 
-completion = openai.ChatCompletion.create(
-    model="gpt-3.5-turbo",
-    messages=[{"role":"user", "content": "What is the circumference of the moon in km?"}]
-)
-
-
-reply_content = completion.choices[0].message.content
-print(reply_content)
-# response = completion['choices'][0]['message']['content']
-
 message_history = []
-user_input = input(">: ")
 
-print("User's input was", user_input)
+def chat(inp, role="user"):
+    message_history.append({"role":role, "content":inp})
+    completion = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=message_history,
+    )
+    reply_content = completion.choices[0].message.content
+    print(reply_content)
+    message_history.append(({"role": "assistant", "content": reply_content}))
+    return reply_content
 
-message_history.append(({"role": "assistant", "content": user_input}))
+while True:
+    user_input = input("Text: ")
+    print("Your text", user_input)
+    print()
+    chat(user_input)
+    print()
 
-completion = openai.ChatCompletion.create(
-    model="gpt-3.5-turbo",
-    messages=message_history,
-)
-
-reply_content = completion.choices[0].message.content
-print(reply_content)
-
-user_input = input(">:")
-print("User's input was", user_input)
-print()
-message_history.append(({"role": "user", "content": user_input}))
-completion = openai.ChatCompletion.create(
-    model="gpt-3.5-turbo",
-    messages=message_history,
-)
-reply_content = completion.choices[0].message.content
-print(reply_content)
